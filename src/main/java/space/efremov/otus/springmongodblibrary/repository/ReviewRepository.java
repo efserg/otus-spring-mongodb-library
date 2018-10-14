f386e183268e5329addc747da3d950aa4cbef144
Sergey Y. Efremov <mynog@yandex.ru>
2018-10-14 18:27:23 +0300

    Update repos to the reactive stack

--- src/main/java/space/efremov/otus/springmongodblibrary/repository/ReviewRepository.java
+++ src/main/java/space/efremov/otus/springmongodblibrary/repository/ReviewRepository.java
@@ -1,8 +1,7 @@
 package space.efremov.otus.springmongodblibrary.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import space.efremov.otus.springmongodblibrary.domain.Author;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import space.efremov.otus.springmongodblibrary.domain.Review;
 
-public interface ReviewRepository extends MongoRepository<Review, String> {
+public interface ReviewRepository extends ReactiveMongoRepository<Review, String> {
 }
